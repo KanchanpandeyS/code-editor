e5124a9ef624944603e101fa31e3a9e406aeb1f5
Kanchan <kanchanpandey6179@gmail.com>
2022-12-08 19:41:56 +0530

    updated All

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import header from "./Images/Header.jpg";
+
+export default function Header() {
+  return (
+    <div id="header">
+      <img src={header} alt="" height="70px" />
+      <h2 id="title">Code Editor</h2>
+      {/* <p>By Kanchan Pnadewy</p> */}
+    </div>
+  );
+}
